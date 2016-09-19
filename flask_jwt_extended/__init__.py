6abc4d227f5ed8a01d6e72f6e98fbd88bee38c07
Landon GB <landogbland@gmail.com>
2016-09-19 10:37:11 -0600

    Refactor blacklist functions into seperate file

--- flask_jwt_extended/__init__.py
+++ flask_jwt_extended/__init__.py
@@ -1,5 +1,6 @@
 from .jwt_manager import JWTManager
 from .utils import (jwt_identity, jwt_claims, jwt_required, fresh_jwt_required,
                     create_refresh_access_tokens, refresh_access_token,
-                    create_fresh_access_token, revoke_token, unrevoke_token,
-                    get_stored_tokens)
+                    create_fresh_access_token)
+from .blacklist import (revoke_token, unrevoke_token, get_stored_tokens,
+                        get_all_stored_tokens)

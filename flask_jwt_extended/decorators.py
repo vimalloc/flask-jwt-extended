91fd27bffa176c4f4a9eff7efec258a7aa58a591
Landon GB <landogbland@gmail.com>
2017-04-09 20:35:52 -0600

    Move protected endpoint decorators into new file + small refactors

---
+++ flask_jwt_extended/decorators.py
@@ -0,0 +1,166 @@
+from functools import wraps
+
+from flask import request
+from werkzeug.security import safe_str_cmp
+try:
+    from flask import _app_ctx_stack as ctx_stack
+except ImportError:  # pragma: no cover
+    from flask import _request_ctx_stack as ctx_stack
+
+from flask_jwt_extended.blacklist import check_if_token_revoked
+from flask_jwt_extended.config import config
+from flask_jwt_extended.exceptions import (
+    InvalidHeaderError, NoAuthorizationError, WrongTokenError,
+    FreshTokenRequired, CSRFError
+)
+from flask_jwt_extended.tokens import decode_jwt
+
+
+def jwt_required(fn):
+    """
+    If you decorate a vew with this, it will ensure that the requester has a
+    valid JWT before calling the actual view. This does not check the freshness
+    of the token.
+
+    See also: fresh_jwt_required()
+
+    :param fn: The view function to decorate
+    """
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        # Save the jwt in the context so that it can be accessed later by
+        # the various endpoints that is using this decorator
+        jwt_data = _decode_jwt_from_request(request_type='access')
+        ctx_stack.top.jwt = jwt_data
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+def fresh_jwt_required(fn):
+    """
+    If you decorate a vew with this, it will ensure that the requester has a
+    valid JWT before calling the actual view.
+
+    See also: jwt_required()
+
+    :param fn: The view function to decorate
+    """
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        # Check if the token is fresh
+        jwt_data = _decode_jwt_from_request(request_type='access')
+        if not jwt_data['fresh']:
+            raise FreshTokenRequired('Fresh token required')
+
+        # Save the jwt in the context so that it can be accessed later by
+        # the various endpoints that is using this decorator
+        ctx_stack.top.jwt = jwt_data
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+def jwt_refresh_token_required(fn):
+    """
+    If you decorate a view with this, it will insure that the requester has a
+    valid JWT refresh token before calling the actual view. If the token is
+    invalid, expired, not present, etc, the appropriate callback will be called
+    """
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        # Save the jwt in the context so that it can be accessed later by
+        # the various endpoints that is using this decorator
+        jwt_data = _decode_jwt_from_request(request_type='refresh')
+        ctx_stack.top.jwt = jwt_data
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+def _decode_jwt_from_headers(request_type):
+    if request_type == 'access':
+        header_name = config.access_header_name
+        header_type = config.header_type
+    else:
+        header_name = config.refresh_header_name
+        header_type = config.header_type
+
+    # Verify we have the auth header
+    jwt_header = request.headers.get(header_name, None)
+    if not jwt_header:
+        raise NoAuthorizationError("Missing {} Header".format(header_name))
+
+    # Make sure the header is in a valid format that we are expecting, ie
+    # <HeaderName>: <HeaderType(optional)> <JWT>
+    parts = jwt_header.split()
+    if not header_type:
+        if len(parts) != 1:
+            msg = "Bad {} header. Expected value '<JWT>'".format(header_name)
+            raise InvalidHeaderError(msg)
+        token = parts[0]
+    else:
+        if parts[0] != header_type or len(parts) != 2:
+            msg = "Bad {} header. Expected value '{} <JWT>'".format(header_name, header_type)
+            raise InvalidHeaderError(msg)
+        token = parts[1]
+
+    return decode_jwt(token, config.secret_key, config.algorithm, csrf=False)
+
+
+def _decode_jwt_from_cookies(request_type):
+    if request_type == 'access':
+        cookie_key = config.access_cookie_name
+        csrf_header_key = config.access_csrf_header_name
+    else:
+        cookie_key = config.refresh_cookie_name
+        csrf_header_key = config.refresh_csrf_header_name
+
+    encoded_token = request.cookies.get(cookie_key)
+    if not encoded_token:
+        raise NoAuthorizationError('Missing cookie "{}"'.format(cookie_key))
+
+    decoded_token = decode_jwt(
+        encoded_token=encoded_token,
+        secret=config.secret_key,
+        algorithm=config.algorithm,
+        csrf=config.csrf_protect
+    )
+
+    # Verify csrf double submit tokens match if required
+    if config.csrf_protect and request.method in config.csrf_request_methods:
+        csrf_token_in_token = decoded_token['csrf']
+        csrf_token_in_header = request.headers.get(csrf_header_key, None)
+
+        if not csrf_token_in_header:
+            raise CSRFError("Missing CSRF token in headers")
+        if not safe_str_cmp(csrf_token_in_header, csrf_token_in_token):
+            raise CSRFError("CSRF double submit tokens do not match")
+
+    return decoded_token
+
+
+def _decode_jwt_from_request(request_type):
+    # We have three cases here, having jwts in both cookies and headers is
+    # valid, or the jwt can only be saved in one of cookies or headers. Check
+    # all cases here.
+    if config.jwt_in_cookies and config.jwt_in_headers:
+        try:
+            decoded_token = _decode_jwt_from_cookies(request_type)
+        except NoAuthorizationError:
+            try:
+                decoded_token = _decode_jwt_from_headers(request_type)
+            except NoAuthorizationError:
+                raise NoAuthorizationError("Missing JWT in headers and cookies")
+    elif config.jwt_in_headers:
+        decoded_token = _decode_jwt_from_headers(request_type)
+    else:
+        decoded_token = _decode_jwt_from_cookies(request_type)
+
+    # Make sure the type of token we received matches the request type we expect
+    if decoded_token['type'] != request_type:
+        raise WrongTokenError('Only {} tokens can access this endpoint'.format(request_type))
+
+    # If blacklisting is enabled, see if this token has been revoked
+    if config.blacklist_enabled:
+        check_if_token_revoked(decoded_token)
+
+    return decoded_token
+
--- flask_jwt_extended/utils.py
+++ flask_jwt_extended/utils.py
@@ -1,22 +1,13 @@
-from functools import wraps
-
-from flask import request, current_app
-from werkzeug.security import safe_str_cmp
-
-from flask_jwt_extended.tokens import (
-    decode_jwt, encode_refresh_token, encode_access_token
-)
-
+from flask import current_app
 try:
     from flask import _app_ctx_stack as ctx_stack
 except ImportError:  # pragma: no cover
     from flask import _request_ctx_stack as ctx_stack
 
-from flask_jwt_extended.blacklist import check_if_token_revoked, store_token
+from flask_jwt_extended.blacklist import store_token
 from flask_jwt_extended.config import config
-from flask_jwt_extended.exceptions import (
-    InvalidHeaderError, NoAuthorizationError, WrongTokenError,
-    FreshTokenRequired, CSRFError
+from flask_jwt_extended.tokens import (
+    decode_jwt, encode_refresh_token, encode_access_token
 )
 
 
@@ -44,182 +35,6 @@ def get_raw_jwt():
     return getattr(ctx_stack.top, 'jwt', {})
 
 
-
-def _decode_jwt_from_headers(type):
-    # TODO make type an enum or something instead of a magic string
-    if type == 'access':
-        header_name = config.access_header_name
-        header_type = config.header_type
-    else:
-        header_name = config.refresh_header_name
-        header_type = config.header_type
-
-    # Verify we have the auth header
-    jwt_header = request.headers.get(header_name, None)
-    if not jwt_header:
-        raise NoAuthorizationError("Missing {} Header".format(header_name))
-
-    # Make sure the header is in a valid format that we are expecting, ie
-    # <HeaderName>: <HeaderType(optional)> <JWT>
-    parts = jwt_header.split()
-    if not header_type:
-        if len(parts) != 1:
-            msg = "Bad {} header. Expected value '<JWT>'".format(header_name)
-            raise InvalidHeaderError(msg)
-        token = parts[0]
-    else:
-        if parts[0] != header_type or len(parts) != 2:
-            msg = "Bad {} header. Expected value '{} <JWT>'".format(header_name, header_type)
-            raise InvalidHeaderError(msg)
-        token = parts[1]
-
-    return decode_jwt(token, config.secret_key, config.algorithm, csrf=False)
-
-
-def _decode_jwt_from_cookies(type):
-    # TODO make type an enum or something instead of a magic string
-    if type == 'access':
-        cookie_key = config.access_cookie_name
-        csrf_header_key = config.access_csrf_header_name
-    else:
-        cookie_key = config.refresh_cookie_name
-        csrf_header_key = config.refresh_csrf_header_name
-
-    encoded_token = request.cookies.get(cookie_key)
-    if not encoded_token:
-        raise NoAuthorizationError('Missing cookie "{}"'.format(cookie_key))
-
-    decoded_token = decode_jwt(
-        encoded_token=encoded_token,
-        secret=config.secret_key,
-        algorithm=config.algorithm,
-        csrf=config.csrf_protect
-    )
-
-    # Verify csrf double submit tokens match if required
-    if config.csrf_protect and request.method in config.csrf_request_methods:
-        csrf_token_in_token = decoded_token['csrf']
-        csrf_token_in_header = request.headers.get(csrf_header_key, None)
-
-        if not csrf_token_in_header:
-            raise CSRFError("Missing CSRF token in headers")
-        if not safe_str_cmp(csrf_token_in_header, csrf_token_in_token):
-            raise CSRFError("CSRF double submit tokens do not match")
-
-    return decoded_token
-
-
-def _decode_jwt_from_request(type):
-    # JWT can be in either headers or cookies
-    if config.jwt_in_cookies and config.jwt_in_headers:
-        try:
-            return _decode_jwt_from_headers(type)
-        except NoAuthorizationError:
-            pass
-        try:
-            return _decode_jwt_from_cookies(type)
-        except NoAuthorizationError:
-            pass
-        raise NoAuthorizationError("Missing JWT in header and cookies")
-
-    # JWT can only be in headers
-    elif config.jwt_in_headers:
-        return _decode_jwt_from_headers(type)
-
-    # JWT can only be in cookie
-    else:
-        return _decode_jwt_from_cookies(type)
-
-
-def jwt_required(fn):
-    """
-    If you decorate a vew with this, it will ensure that the requester has a valid
-    JWT before calling the actual view. This does not check the freshness of the
-    token.
-
-    See also: fresh_jwt_required()
-
-    :param fn: The view function to decorate
-    """
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        # Attempt to decode the token
-        jwt_data = _decode_jwt_from_request(type='access')
-
-        # Verify this is an access token
-        if jwt_data['type'] != 'access':
-            raise WrongTokenError('Only access tokens can access this endpoint')
-
-        # If blacklisting is enabled, see if this token has been revoked
-        if config.blacklist_enabled:
-            check_if_token_revoked(jwt_data)
-
-        # Save the jwt in the context so that it can be accessed later by
-        # the various endpoints that is using this decorator
-        ctx_stack.top.jwt = jwt_data
-        return fn(*args, **kwargs)
-    return wrapper
-
-
-def fresh_jwt_required(fn):
-    """
-    If you decorate a vew with this, it will ensure that the requester has a valid
-    JWT before calling the actual view.
-
-    See also: jwt_required()
-
-    :param fn: The view function to decorate
-    """
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        # Attempt to decode the token
-        jwt_data = _decode_jwt_from_request(type='access')
-
-        # Verify this is an access token
-        if jwt_data['type'] != 'access':
-            raise WrongTokenError('Only access tokens can access this endpoint')
-
-        # If blacklisting is enabled, see if this token has been revoked
-        if config.blacklist_enabled:
-            check_if_token_revoked(jwt_data)
-
-        # Check if the token is fresh
-        if not jwt_data['fresh']:
-            raise FreshTokenRequired('Fresh token required')
-
-        # Save the jwt in the context so that it can be accessed later by
-        # the various endpoints that is using this decorator
-        ctx_stack.top.jwt = jwt_data
-        return fn(*args, **kwargs)
-    return wrapper
-
-
-def jwt_refresh_token_required(fn):
-    """
-    If you decorate a view with this, it will insure that the requester has a
-    valid JWT refresh token before calling the actual view. If the token is
-    invalid, expired, not present, etc, the appropriate callback will be called
-    """
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        # Get the JWT
-        jwt_data = _decode_jwt_from_request(type='refresh')
-
-        # verify this is a refresh token
-        if jwt_data['type'] != 'refresh':
-            raise WrongTokenError('Only refresh tokens can access this endpoint')
-
-        # If blacklisting is enabled, see if this token has been revoked
-        if config.blacklist_enabled:
-            check_if_token_revoked(jwt_data)
-
-        # Save the jwt in the context so that it can be accessed later by
-        # the various endpoints that is using this decorator
-        ctx_stack.top.jwt = jwt_data
-        return fn(*args, **kwargs)
-    return wrapper
-
-
 def create_refresh_token(identity):
     """
     Creates a new refresh token
@@ -234,7 +49,6 @@ def create_refresh_token(identity):
                      in the JWT via the user_claims_loader
     :return: A new refresh token
     """
-    # TODO this should be moved to the jwt manager
     refresh_token = encode_refresh_token(
         identity=current_app.jwt_manager._user_identity_callback(identity),
         secret=config.secret_key,
@@ -267,7 +81,6 @@ def create_access_token(identity, fresh=False):
                   fresh_jwt_required protected endpoints. Defaults to False
     :return: A new access token
     """
-    # TODO this should be moved to the jwt manager
     access_token = encode_access_token(
         identity=current_app.jwt_manager._user_identity_callback(identity),
         secret=config.secret_key,
@@ -284,10 +97,8 @@ def create_access_token(identity, fresh=False):
     return access_token
 
 
-def _get_csrf_token(encoded_token):
-    secret = config.secret_key
-    algorithm = config.algorithm
-    token = decode_jwt(encoded_token, secret, algorithm, csrf=True)
+def get_csrf_token(encoded_token):
+    token = decode_jwt(encoded_token, config.secret_key, config.algorithm, csrf=True)
     return token['csrf']
 
 
@@ -311,7 +122,7 @@ def set_access_cookies(response, encoded_access_token):
     # If enabled, set the csrf double submit access cookie
     if config.csrf_protect:
         response.set_cookie(config.access_csrf_cookie_name,
-                            value=_get_csrf_token(encoded_access_token),
+                            value=get_csrf_token(encoded_access_token),
                             max_age=config.cookie_max_age,
                             secure=config.cookie_secure,
                             httponly=False,
@@ -338,7 +149,7 @@ def set_refresh_cookies(response, encoded_refresh_token):
     # If enabled, set the csrf double submit refresh cookie
     if config.csrf_protect:
         response.set_cookie(config.refresh_csrf_cookie_name,
-                            value=_get_csrf_token(encoded_refresh_token),
+                            value=get_csrf_token(encoded_refresh_token),
                             max_age=config.cookie_max_age,
                             secure=config.cookie_secure,
                             httponly=False,
@@ -381,5 +192,3 @@ def unset_jwt_cookies(response):
                             secure=config.cookie_secure,
                             httponly=False,
                             path='/')
-
-    return response

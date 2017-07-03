0b437e32f31b9a48d70d9a20a3ebc6d8108cf914
Landon GB <landogbland@gmail.com>
2017-07-02 18:08:31 -0600

    Update example for new blacklist. Will add more in depth examples soon

--- examples/blacklist.py
+++ examples/blacklist.py
@@ -1,36 +1,46 @@
-import datetime
-
-import simplekv.memory
 from flask import Flask, request, jsonify
 
-from flask_jwt_extended import JWTManager, jwt_required, \
-    get_jwt_identity, revoke_token, unrevoke_token, \
-    get_stored_tokens, get_all_stored_tokens, create_access_token, \
-    create_refresh_token, jwt_refresh_token_required, \
-    get_raw_jwt, get_stored_token
+from flask_jwt_extended import (
+    JWTManager, jwt_required, get_jwt_identity,
+    create_access_token, create_refresh_token,
+    jwt_refresh_token_required, get_raw_jwt
+)
 
 
 # Setup flask
 app = Flask(__name__)
 app.secret_key = 'super-secret'
 
-# Enable and configure the JWT blacklist / token revoke. We are using
-# an in memory store for this example. In production, you should
-# use something persistent (such as redis, memcached, sqlalchemy).
-# See here for options: http://pythonhosted.org/simplekv/
+# Enable blacklisting and specify what kind of tokens to check
+# against the blacklist
 app.config['JWT_BLACKLIST_ENABLED'] = True
-app.config['JWT_BLACKLIST_STORE'] = simplekv.memory.DictStore()
-
-# Check all tokens (access and refresh) to see if they have been revoked.
-# You can alternately check only the refresh tokens here, by setting this
-# to 'refresh' instead of 'all'
-app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = 'all'
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(minutes=5)
-
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 jwt = JWTManager(app)
 
+# A storage engine to save revoked tokens. In production if
+# speed is the primary concern, redis is a good bet. If data
+# persistence is more important for you, postgres is another
+# great option. In this example, we will be using an in memory
+# store, just to show you how this might work
+blacklist = set()
 
-# Standard login endpoint
+
+# For this example, we are just checking if the tokens jti
+# (unique identifier) is in the blacklist set. This could
+# be made more complex, for example storing all tokens
+# into the blacklist with a revoked status when created,
+# and returning the revoked status in this call. This
+# would allow you to have a list of all created tokens,
+# and to consider tokens that aren't in the blacklist
+# (aka tokens you didn't create) as revoked. These are
+# just two options, and this can be tailored to whatever
+#  your application needs.
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return jti in blacklist
+
+
 @app.route('/login', methods=['POST'])
 def login():
     username = request.json.get('username', None)
@@ -45,7 +55,6 @@ def login():
     return jsonify(ret), 200
 
 
-# Standard refresh endpoint
 @app.route('/refresh', methods=['POST'])
 @jwt_refresh_token_required
 def refresh():
@@ -56,24 +65,12 @@ def refresh():
     return jsonify(ret), 200
 
 
-# Helper method to revoke the current token used to access
-# a protected endpoint
-def _revoke_current_token():
-    current_token = get_raw_jwt()
-    jti = current_token['jti']
-    revoke_token(jti)
-
-
 # Endpoint for revoking the current users access token
 @app.route('/logout', methods=['POST'])
 @jwt_required
 def logout():
-    try:
-        _revoke_current_token()
-    except KeyError:
-        return jsonify({
-            'msg': 'Access token not found in the blacklist store'
-        }), 500
+    jti = get_raw_jwt()['jti']
+    blacklist.add(jti)
     return jsonify({"msg": "Successfully logged out"}), 200
 
 
@@ -81,62 +78,13 @@ def logout():
 @app.route('/logout2', methods=['POST'])
 @jwt_refresh_token_required
 def logout2():
-    try:
-        _revoke_current_token()
-    except KeyError:
-        return jsonify({
-            'msg': 'Refresh token not found in the blacklist store'
-        }), 500
+    jti = get_raw_jwt()['jti']
+    blacklist.add(jti)
     return jsonify({"msg": "Successfully logged out"}), 200
 
 
-# Endpoint for listing tokens that have the same identity as you
-# NOTE: This is currently very inefficient.
-@app.route('/auth/tokens', methods=['GET'])
-@jwt_required
-def list_identity_tokens():
-    username = get_jwt_identity()
-    return jsonify(get_stored_tokens(username)), 200
-
-
-# Endpoint for listing all tokens. In your app, you should either
-# not expose this endpoint, or put some addition security on top
-# of it so only trusted users (administrators, etc) can access it
-@app.route('/auth/all-tokens')
-def list_all_tokens():
-    return jsonify(get_all_stored_tokens()), 200
-
-
-# Endpoint for allowing users to revoke their own tokens.
-@app.route('/auth/tokens/revoke/<string:jti>', methods=['PUT'])
-@jwt_required
-def change_jwt_revoke_state(jti):
-    username = get_jwt_identity()
-    try:
-        token_data = get_stored_token(jti)
-        if token_data['token']['identity'] != username:
-            raise KeyError
-        revoke_token(jti)
-        return jsonify({"msg": "Token successfully revoked"}), 200
-    except KeyError:
-        return jsonify({'msg': 'Token not found'}), 404
-
-
-# Endpoint for allowing users to un-revoke their own tokens.
-@app.route('/auth/tokens/unrevoke/<string:jti>', methods=['PUT'])
-@jwt_required
-def change_jwt_unrevoke_state(jti):
-    username = get_jwt_identity()
-    try:
-        token_data = get_stored_token(jti)
-        if token_data['token']['identity'] != username:
-            raise KeyError
-        unrevoke_token(jti)
-        return jsonify({"msg": "Token successfully unrevoked"}), 200
-    except KeyError:
-        return jsonify({'msg': 'Token not found'}), 404
-
-
+# This will now prevent users with blacklisted tokens from
+# accessing this endpoint
 @app.route('/protected', methods=['GET'])
 @jwt_required
 def protected():

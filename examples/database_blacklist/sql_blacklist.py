ece432200e426656dbb608420d6576a2d53815b2
Landon GB <landogbland@gmail.com>
2017-07-04 16:55:31 -0600

    Start working on database example for blacklist

---
+++ examples/database_blacklist/sql_blacklist.py
@@ -0,0 +1,5 @@
+# Method for pruning database
+# Method for getting tokens for a specific user
+# Method for adding a token to the database
+# Method for revoking a token
+# Method for unrevoking a token

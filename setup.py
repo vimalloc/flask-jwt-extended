9a68425e039039c57ea3578f6001664f96983bec
Landon GB <landogbland@gmail.com>
2017-07-08 19:12:37 -0600

    Bump to 3.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Flask-Login provides jwt endpoint protection for Flask.
 from setuptools import setup
 
 setup(name='Flask-JWT-Extended',
-      version='2.4.1',
+      version='3.0.0',
       url='https://github.com/vimalloc/flask-jwt-extended',
       license='MIT',
       author='Landon Gilbert-Bland',

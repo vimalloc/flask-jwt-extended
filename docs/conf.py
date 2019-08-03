f39a679b57cae9198dafab174a028febef97802a
Landon Gilbert-Bland <landogbland@gmail.com>
2019-08-03 09:07:53 -0600

    Migrate to pallets-sphinx-themes
    Closes #261

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '_themes'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'pallets_sphinx_themes',
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',

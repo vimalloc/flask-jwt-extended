355b1ec18fed1212535774cf1265588db0e841d7
Landon Gilbert-Bland <lbland@instructure.com>
2021-01-19 13:25:27 -0700

    Add documentation for view decorators

--- flask_jwt_extended/view_decorators.py
+++ flask_jwt_extended/view_decorators.py
@@ -35,9 +35,19 @@ def _verify_token_is_fresh(jwt_header, jwt_data):
 
 def verify_jwt_in_request(optional=False, fresh=False, refresh=False, locations=None):
     """
-    Ensure that the requester has a valid access token. This does not check the
-    freshness of the access token. Raises an appropiate exception there is
-    no token or if the token is invalid.
+    Verify that a valid JWT is present in the request, unless `optional=True` in
+    which case no JWT is also considered valid.
+
+    :param optional: If `True`, do not raise an error if no JWT is present in
+                     the request. Defaults to `False`.
+    :param fresh: If `True`, require a JWT marked as `fresh` in order to be
+                  verified. Defaults to `False`.
+    :param refresh: If `True`, require a refresh JWT to be verified. If `False`
+                    require an access JWT to be verified. Defaults to `False`.
+    :param locations: A list of locations to look for the JWT in this request,
+                      for example: `['headers', 'cookies']`. Defaluts to `None`
+                      which indicates that JWTs will be looked for in the locations
+                      defined by the `JWT_TOKEN_LOCATION` configuration option.
     """
     if request.method in config.exempt_methods:
         return
@@ -70,13 +80,23 @@ def verify_jwt_in_request(optional=False, fresh=False, refresh=False, locations=
 
 def jwt_required(optional=False, fresh=False, refresh=False, locations=None):
     """
-    A decorator to protect a Flask endpoint.
+    A decorator to protect a Flask endpoint with JSON Web Tokens.
 
-    If you decorate an endpoint with this, it will ensure that the requester
-    has a valid access token before allowing the endpoint to be called. This
-    does not check the freshness of the access token.
+    Any route decorated with this will require a valid JWT to be present in the
+    request (unless optional=True, in which case no JWT is also valid) before the
+    endpoint can be called.
 
-    See also: :func:`~flask_jwt_extended.fresh_jwt_required`
+    :param optional: If `True`, allow the decorated endpoint to be if no JWT is
+                     present in the request. Defaults to `False`.
+    :param fresh: If `True`, require a JWT marked with `fresh` to be able to
+                  access this endpoint. Defaults to `False`.
+    :param refresh: If `True`, requires a refresh JWT to access this endpoint.
+                    If `False`, requires an access JWT to access this endpoint.
+                    defaults to `False`.
+    :param locations: A list of locations to look for the JWT in this request,
+                      for example: `['headers', 'cookies']`. Defaluts to `None`
+                      which indicates that JWTs will be looked for in the locations
+                      defined by the `JWT_TOKEN_LOCATION` configuration option.
     """
 
     def wrapper(fn):

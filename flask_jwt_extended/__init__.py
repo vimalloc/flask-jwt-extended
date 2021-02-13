9b23a5debc6da186403f942c9eabfbd82d6b0ebd
Landon Gilbert-Bland <lbland@instructure.com>
2021-02-13 13:29:52 -0700

    bump to 4.0.1 to fix documentation and automatic deployment

--- flask_jwt_extended/__init__.py
+++ flask_jwt_extended/__init__.py
@@ -18,4 +18,4 @@ from .utils import unset_refresh_cookies
 from .view_decorators import jwt_required
 from .view_decorators import verify_jwt_in_request
 
-__version__ = "4.0.0"
+__version__ = "4.0.1"

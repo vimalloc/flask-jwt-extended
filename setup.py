6ef47a7e013a964b91820e8e01b425f03bc6711a
Landon Gilbert-Bland <lbland@instructure.com>
2021-02-13 13:20:30 -0700

    Fix documentation build and deploy issues

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only"
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",

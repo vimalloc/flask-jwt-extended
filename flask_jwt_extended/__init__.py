693431a5cb35e81fc1a5a54190792e070d3ff550
Konstantinos Smanis <konstantinos.smanis@gmail.com>
2022-06-25 12:49:09 +0300

    fix: explicitly mark exported names
    Fixes compatibility with the [`--no-implicit-reexport`][1] mypy flag in
    downstream projects. The flag is commonly turned on when the downstream
    project makes use of mypy [strict mode][2].
    
    [1]: https://mypy.readthedocs.io/en/stable/command_line.html#cmdoption-mypy-no-implicit-reexport
    [2]: https://mypy.readthedocs.io/en/stable/command_line.html#cmdoption-mypy-strict
    
    Signed-off-by: Konstantinos Smanis <konstantinos.smanis@gmail.com>

--- flask_jwt_extended/__init__.py
+++ flask_jwt_extended/__init__.py
@@ -1,22 +1,22 @@
-from .jwt_manager import JWTManager
-from .utils import create_access_token
-from .utils import create_refresh_token
-from .utils import current_user
-from .utils import decode_token
-from .utils import get_csrf_token
-from .utils import get_current_user
-from .utils import get_jti
-from .utils import get_jwt
-from .utils import get_jwt_header
-from .utils import get_jwt_identity
-from .utils import get_jwt_request_location
-from .utils import get_unverified_jwt_headers
-from .utils import set_access_cookies
-from .utils import set_refresh_cookies
-from .utils import unset_access_cookies
-from .utils import unset_jwt_cookies
-from .utils import unset_refresh_cookies
-from .view_decorators import jwt_required
-from .view_decorators import verify_jwt_in_request
+from .jwt_manager import JWTManager as JWTManager
+from .utils import create_access_token as create_access_token
+from .utils import create_refresh_token as create_refresh_token
+from .utils import current_user as current_user
+from .utils import decode_token as decode_token
+from .utils import get_csrf_token as get_csrf_token
+from .utils import get_current_user as get_current_user
+from .utils import get_jti as get_jti
+from .utils import get_jwt as get_jwt
+from .utils import get_jwt_header as get_jwt_header
+from .utils import get_jwt_identity as get_jwt_identity
+from .utils import get_jwt_request_location as get_jwt_request_location
+from .utils import get_unverified_jwt_headers as get_unverified_jwt_headers
+from .utils import set_access_cookies as set_access_cookies
+from .utils import set_refresh_cookies as set_refresh_cookies
+from .utils import unset_access_cookies as unset_access_cookies
+from .utils import unset_jwt_cookies as unset_jwt_cookies
+from .utils import unset_refresh_cookies as unset_refresh_cookies
+from .view_decorators import jwt_required as jwt_required
+from .view_decorators import verify_jwt_in_request as verify_jwt_in_request
 
 __version__ = "4.4.1"

01693b58cc6fc9cc4733693262d76c3c0313c374
aoright <102943475+aoright@users.noreply.github.com>
2026-08-20 17:31:06 +0800

    docs: fix typos and grammar in exceptions docstrings
    Signed-off-by: aoright <102943475+aoright@users.noreply.github.com>

--- flask_jwt_extended/exceptions.py
+++ flask_jwt_extended/exceptions.py
@@ -1,6 +1,6 @@
 class JWTExtendedException(Exception):
     """
-    Base except which all flask_jwt_extended errors extend
+    Base exception which all flask_jwt_extended errors extend.
     """
 
     pass
@@ -8,7 +8,7 @@ class JWTExtendedException(Exception):
 
 class JWTDecodeError(JWTExtendedException):
     """
-    An error decoding a JWT
+    An error decoding a JWT.
     """
 
     pass
@@ -16,7 +16,7 @@ class JWTDecodeError(JWTExtendedException):
 
 class InvalidHeaderError(JWTExtendedException):
     """
-    An error getting header information from a request
+    An error getting header information from a request.
     """
 
     pass
@@ -24,7 +24,7 @@ class InvalidHeaderError(JWTExtendedException):
 
 class InvalidQueryParamError(JWTExtendedException):
     """
-    An error when a query string param is not in the correct format
+    An error when a query string param is not in the correct format.
     """
 
     pass
@@ -32,7 +32,7 @@ class InvalidQueryParamError(JWTExtendedException):
 
 class NoAuthorizationError(JWTExtendedException):
     """
-    An error raised when no authorization token was found in a protected endpoint
+    An error raised when no authorization token was found in a protected endpoint.
     """
 
     pass
@@ -40,7 +40,7 @@ class NoAuthorizationError(JWTExtendedException):
 
 class CSRFError(JWTExtendedException):
     """
-    An error with CSRF protection
+    An error with CSRF protection.
     """
 
     pass
@@ -49,7 +49,7 @@ class CSRFError(JWTExtendedException):
 class WrongTokenError(JWTExtendedException):
     """
     Error raised when attempting to use a refresh token to access an endpoint
-    or vice versa
+    or vice versa.
     """
 
     pass
@@ -57,7 +57,7 @@ class WrongTokenError(JWTExtendedException):
 
 class RevokedTokenError(JWTExtendedException):
     """
-    Error raised when a revoked token attempt to access a protected endpoint
+    Error raised when a revoked token attempts to access a protected endpoint.
     """
 
     def __init__(self, jwt_header: dict, jwt_data: dict) -> None:
@@ -68,8 +68,8 @@ class RevokedTokenError(JWTExtendedException):
 
 class FreshTokenRequired(JWTExtendedException):
     """
-    Error raised when a valid, non-fresh JWT attempt to access an endpoint
-    protected by fresh_jwt_required
+    Error raised when a valid, non-fresh JWT attempts to access an endpoint
+    protected by fresh_jwt_required.
     """
 
     def __init__(self, message, jwt_header: dict, jwt_data: dict) -> None:
@@ -93,7 +93,7 @@ class UserLookupError(JWTExtendedException):
 class UserClaimsVerificationError(JWTExtendedException):
     """
     Error raised when the claims_verification_callback function returns False,
-    indicating that the expected user claims are invalid
+    indicating that the expected user claims are invalid.
     """
 
     def __init__(self, message, jwt_header: dict, jwt_data: dict) -> None:
